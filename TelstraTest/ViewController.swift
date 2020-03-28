7b2eeef39124f3e8beedab7caef0bc0d7d431c22
saurabh suman <saurabh.msuman@gmail.com>
2020-03-28 15:44:59 +0530

    Initial commit created empty project

---
+++ TelstraTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TelstraTest
+//
+//  Created by saurabh suman on 28/03/20.
+//  Copyright © 2020 saurabh suman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+    
+    }
+
+
+}
+
